54dd45ea58ca54d10ab7961eec60a7b0ccdb9fe1
redyee925 <37134109+redyee925@users.noreply.github.com>
2018-03-14 15:54:36 +0800

    HW1

---
+++ hw1/hw1.ino
@@ -0,0 +1,32 @@
+const byte led[]={5,4,13,12,11,10,9,8};
+
+void setup() {
+
+for (byte i = 0; i<= 7; i++) {
+pinMode(led[i], OUTPUT);
+}
+}
+//byte lightPin = startPin;
+
+void loop()
+{
+byte i;
+
+
+
+for(i=0;i<=7;i++)
+{
+  digitalWrite(led[i], HIGH);
+  delay (200);
+  digitalWrite(led[i], LOW);
+  delay (200);
+}
+  for(i=7;i>0;i--)
+{
+  digitalWrite(led[i], HIGH);
+  delay (200);
+  digitalWrite(led[i], LOW);
+  delay (200);
+}
+}
+
